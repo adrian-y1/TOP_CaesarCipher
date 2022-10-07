d7d6badbd868cb52b5eac4e6e5a85b2eb9401d8b
adrian-y1 <adrian.yonan1@gmail.com>
2022-10-07 20:44:02 +1100

    Testing with positive shift factor

--- spec/caesar_spec.rb
+++ spec/caesar_spec.rb
@@ -5,5 +5,13 @@ describe 'Caesar Cipher' do
         it 'works with negative shift factors' do
             expect(caesar_cipher('s', -5)).to eql('n')
         end
+
+        it 'works with positive shift factor' do
+            expect(caesar_cipher('s', 5)).to eql('x')
+        end
+
+        it 'it works with any letter casing' do
+            expect(caesar_cipher('HeLLo', 3)).to eql('KhOOr')
+        end
     end
 end
